945e1ddc84a618fc5b4585c31cb007bc04c1f326
JHJ <wjs3662@naver.com>
2019-10-05 02:43:44 +0900

    2
    esc 종료

--- 프로젝트/project.py
+++ 프로젝트/project.py
@@ -1,9 +1,30 @@
 from pico2d import *
 
-open_canvas(1280, 720)
+width, height = 1280, 720
+
+open_canvas(width, height)
 map = load_image('map.png')
 character = load_image('peashooter.png')
 
-map.draw_now(640, 360)
-character.draw_now(300, 100)
+running = True
+frame = 0
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    pass
+
+while running:
+    clear_canvas()
+    map.draw(width//2, height//2)
+    character.draw(300, 100)
+    update_canvas()
+    frame = (frame + 1) % 8
+    handle_events()
+    
+close_canvas()
