c8d4544fd4c5c950deba64e82d36258f9ce2a6bb
JHJ <wjs3662@naver.com>
2019-11-10 23:52:33 +0900

    강화 좀비 수정

--- 프로젝트/strong_zombie.py
+++ 프로젝트/strong_zombie.py
@@ -1,19 +1,29 @@
 from pico2d import *
+from bullet import Bullet
 import random
 
-class Strong_zombie:
+import game_world
+
+class Normal_zombie:
     def __init__(self):
-        self.image = load_image('image/strong_zombie.png')
-        self.x = random.randint (1152, 2000)
+        self.image = load_image('image/walk_normal_zombie.png')
+        self.x = 1200
         self.y = -25 + (random.randint(1, 5) * 96)
-        self.frame = random.randint(0, 7)
+        self.velocity = 0
+        self.frame = 0
+
+    def get_bb(self):
+        return self.x - 50, self.y - 50, self.x + 50, self.y + 50
 
     def update(self):
         self.frame = (self.frame + 1) % 8
-        self.x -= 0.5
+        self.x -= 3
+        delay(0.06)
 
     def draw(self):
         if (self.x > 336):
-            self.image.clip_draw(self.frame * 100, 0, 120, 133, self.x, self.y)
+            self.image.clip_draw(self.frame * 110, 0, 110, 133, self.x, self.y)
         else:
             self.image.draw(336, self.y)
+
+
