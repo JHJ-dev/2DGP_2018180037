c7c53660d4ce074b59f41498ca132914284ca1d3
JHJ <wjs3662@naver.com>
2019-11-13 23:03:46 +0900

    충돌체크 수정

--- 프로젝트/main_state.py
+++ 프로젝트/main_state.py
@@ -22,15 +22,6 @@ Monster = []
 sun_timer = 0.0
 mob_timer = 0.0
 
-def collide(a, b):
-    left_a, bottom_a, right_a, top_a = a.get_bb()
-    left_b, bottom_b, right_b, top_b = b.get_bb()
-    if left_a > right_b: return False
-    if right_a < left_b: return False
-    if top_a < bottom_b: return False
-    if bottom_a > top_b: return False
-    return True
-
 def enter():
     global sun_timer, mob_timer
     background = Background()
