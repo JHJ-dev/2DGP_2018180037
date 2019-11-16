9ed76daf785420a29b7cdb2e3bd9d74e70e63dfa
JHJ <wjs3662@naver.com>
2019-11-17 02:10:30 +0900

    Update mygame.py

--- 프로젝트/mygame.py
+++ 프로젝트/mygame.py
@@ -2,8 +2,6 @@ from pico2d import *
 import game_framework
 import start_state
 
-width, height  = 1152, 672
-
-open_canvas(width, height, sync = True)
+open_canvas(1152, 672)
 game_framework.run(start_state)
 pico2d.close_canvas()
